23eb4aaad8b027a9156e4174d04ecf08a83a433e
NoorTushar <tushar.noor.khan@gmail.com>
2024-01-30 01:23:54 +0600

    different problems using loop

---
+++ loop/different-problems.js
@@ -0,0 +1,15 @@
+// odd numbers 1 to 20
+
+for (i = 1; i <= 20; i++) {
+   if (i % 2 !== 0) {
+      console.log(`it is odd ${i}`);
+   }
+}
+
+// give numbers from 1 to 50 which are divisible by 5
+
+for (let i = 1; i <= 50; i++) {
+   if (i % 5 === 0) {
+      console.log(`The ${i} is divisible by 5`);
+   }
+}
