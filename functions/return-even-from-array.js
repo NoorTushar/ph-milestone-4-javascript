f5244d0d9993ae9734ca2b190b530ee027349772
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-03 00:51:00 +0600

    function-find even and sum

--- functions/return-even-from-array.js
+++ functions/return-even-from-array.js
@@ -1,10 +1,9 @@
 // return SUM of all even numbers from an array.
-
 const numbers = [1, 3, 4, 5, 6, 8, 16];
 
 function addEvenOnly(arr) {
    let sum = 0;
-   for (item of arr) {
+   for (const item of arr) {
       if (item % 2 === 0) {
          sum = sum + item;
       }
