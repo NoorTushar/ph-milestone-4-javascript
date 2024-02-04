29a74e58db168bc53eb7f4dbdcda598a7baa5f6e
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-04 19:08:11 +0600

    get min done

--- module-22-simple-problems-2/get-min.js
+++ module-22-simple-problems-2/get-min.js
@@ -2,4 +2,18 @@
  * Get min number from an array
  */
 
-const numbers = [5, 7, 2, 4, 8, 1];
+const numbers = [5, 7, -2, 1, 2, 4, 8];
+
+function getMin(arr) {
+   let min = numbers[0];
+
+   for (number of numbers) {
+      if (number < min) {
+         min = number;
+      }
+   }
+   return min;
+}
+
+const min = getMin(numbers);
+console.log(min);
