ee1cb852d8d93ee756f413d1781775906a15d28b
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-04 19:04:24 +0600

    max number done

---
+++ module-22-simple-problems-2/get-max.js
@@ -0,0 +1,20 @@
+/**
+ * Get max number from an array
+ */
+
+const numbers = [5, 7, 2, 4, 8, 1];
+
+function getMax(arr) {
+   let max = arr[0];
+
+   for (number of numbers) {
+      if (number > max) {
+         max = number;
+      }
+   }
+
+   return max;
+}
+
+const max = getMax(numbers);
+console.log(max);
---
+++ module-22-simple-problems-2/get-min.js
@@ -0,0 +1,5 @@
+/**
+ * Get min number from an array
+ */
+
+const numbers = [5, 7, 2, 4, 8, 1];
