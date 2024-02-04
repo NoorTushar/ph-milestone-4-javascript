4745c58d815607e0bd249e246ffef36d90a3735b
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-04 15:36:10 +0600

    math random number 20-30 done

--- module-21-simple-problems/math.js
+++ module-21-simple-problems/math.js
@@ -6,3 +6,15 @@
 // Math.ceil()
 // Math.floor()
 // Math.random()
+
+/**
+ * Create a function to generate random number from 20-30
+ */
+
+function generateRandom() {
+   let random = 20 + Math.floor(Math.random() * 11);
+   return random;
+}
+
+const randomNumber = generateRandom();
+console.log(randomNumber);
