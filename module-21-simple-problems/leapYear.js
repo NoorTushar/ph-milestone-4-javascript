d39adc90008b36f359dbdeb48789523ebe0f74af
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-04 14:12:34 +0600

    largest word done

---
+++ module-21-simple-problems/leapYear.js
@@ -0,0 +1,20 @@
+// condition for leap year
+
+/**
+ * 1. Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100
+ * 2. However, if a year is divisible by 400, then it is still considered a leap year
+ **/
+
+/**
+ * Case 1: 2000 - leap year
+ * Case 2: 1600 - leap year
+ * Case 3: 1700 - not leap year
+ * Case 4: 1800 - not leap year
+ * Case 4: 1900 - not leap year
+ **/
+
+let year = 2000;
+
+function isLeapYear(year) {}
+
+console.log(isLeapYear(year));
