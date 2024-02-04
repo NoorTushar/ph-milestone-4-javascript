bd85aa2b0888cce4ec78929d703e653a9b085baf
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-04 19:34:01 +0600

    wood requirement done

---
+++ module-22-simple-problems-2/wood-requirement.js
@@ -0,0 +1,22 @@
+/**
+ * wood needed for:
+ * 1 chair = 3 cft
+ * 1 table = 10 cft
+ * 1 bed = 50 cft
+ */
+
+function woodRequirement(chairQty, tableQty, bedQty) {
+   const perChairWood = 3;
+   const perTableWood = 10;
+   const perBedWood = 50;
+
+   const chairAmount = perChairWood * chairQty;
+   const tableAmount = perTableWood * tableQty;
+   const bedAmount = perBedWood * bedQty;
+
+   const totalWoodRequirement = chairAmount + tableAmount + bedAmount;
+   return totalWoodRequirement;
+}
+
+const woodNeeded = woodRequirement(10, 15, 20);
+console.log(woodNeeded);
