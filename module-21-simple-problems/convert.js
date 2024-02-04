e186e1b5ba27fa2d38c231df85ae9847d7dc3677
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-04 15:01:47 +0600

    conversion done

--- module-21-simple-problems/convert.js
+++ module-21-simple-problems/convert.js
@@ -1,3 +1,15 @@
 // inch to feet
+/**
+ * 12 inch = 1 feet
+ * show the output in x:feet and y:inches format
+ */
 
-function inchToFeet(inch) {}
+function inchToFeet(inches) {
+   const feet = Math.floor(inches / 12);
+   const inch = inches % 12;
+   console.log(feet);
+   console.log(inch);
+   return `Length is ${feet} feet and ${inch} inch`;
+}
+
+console.log(inchToFeet(12));
